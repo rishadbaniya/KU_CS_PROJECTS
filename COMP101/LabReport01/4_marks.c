c3ee2908034e4715f39dcd1cdd15e51510e12f06
rishadbaniya <rbdroid01@gmail.com>
2022-04-08 16:39:31 +0530

    added fourth of report01

---
+++ COMP101/LabReport01/4_marks.c
@@ -0,0 +1,32 @@
+// 4. Write a program that reads the marks in each subject and calculates the percentage.
+// Assume 3 subjects have to be read from user
+
+
+// I'll assume full marks for each subject as 100
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float phy, chem, math, total_obtained, perc;
+
+    printf("Enter marks for PHYSICS - ");
+    scanf("%f", &phy);
+
+    printf("\nEnter marks for CHEMISTRY - ");
+    scanf("%f", &chem);
+
+    printf("\nEnter marks for MATH - ");
+    scanf("%f", &math);
+
+    total_obtained = phy + chem + math;
+    perc = (total_obtained / 300) * 100;
+
+    printf("\nPercentage Obtained - %0.2f %%", perc);
+
+    return 0;
+}
+
+
+
+
